7edc22e42fd33a6079d183372b3f8b058925f0c4
lhp97 <lhdprado@gmail.com>
2023-03-23 17:17:42 -0300

    Routes

--- frontend-dsmovie/src/App.tsx
+++ frontend-dsmovie/src/App.tsx
@@ -1,10 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from "pages/Listing/listing";
+import Form from "pages/Form/form";
 import Navbar from "./src/../components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar/>
-    </div>
+      <Routes>
+        <Route path="/" element={<Listing/>}/>
+        <Route path="/form">
+          <Route path=":movieId" element={<Form/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
