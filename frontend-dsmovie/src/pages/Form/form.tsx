f23789f3eb37039fe524d3589f58ffcad3e36ce3
lhp97 <lhdprado@gmail.com>
2023-04-03 20:58:07 -0300

    Ajust Css form

--- frontend-dsmovie/src/pages/Form/form.tsx
+++ frontend-dsmovie/src/pages/Form/form.tsx
@@ -13,7 +13,7 @@ function Form() {
 
     return (
         <div className="dsmovie-form-container">
-            <img className="dsmovie-card-image" src={movie.image} alt={movie.title} />
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <form className="dsmovie-form">
