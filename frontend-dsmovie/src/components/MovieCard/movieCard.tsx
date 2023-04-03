de0dca4acbeede5efb932082eb58cc0e677c0b4a
lhp97 <lhdprado@gmail.com>
2023-04-03 07:54:09 -0300

    movie card completed

---
+++ frontend-dsmovie/src/components/MovieCard/movieCard.tsx
@@ -0,0 +1,25 @@
+import MovieScore from "components/MovieScore/movieScore";
+
+function MovieCard() {
+  const movie = {
+    id: 1,
+    image:
+      "https://occ-0-3934-114.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABVWS9pqT4D1VP8vlUp4tpjXcO0T2xb7QHYJAZgmwalMcwqgHKJNUEZdbgwOrydpkKgTrgA-hRPGQh5XYsNydSOMvYCsV1Vnm808M.jpg?r=93a",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
+  return (
+    <div>
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <MovieScore />
+        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
--- frontend-dsmovie/src/pages/Listing/listing.tsx
+++ frontend-dsmovie/src/pages/Listing/listing.tsx
@@ -1,9 +1,19 @@
+import MovieCard from "components/MovieCard/movieCard";
 import Pagination from "components/Pagination/pagination";
 
 function Listing() {
-    return (
-        <Pagination/>
-    );
+  return (
+    <div>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Listing;
