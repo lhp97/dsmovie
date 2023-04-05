21562a7a0336dea1b7d9a1e27c8a8b7569e73f2a
lhp97 <lhdprado@gmail.com>
2023-04-05 08:30:22 -0300

    Find movie by id created

--- backend-dsmovie/src/main/java/br/com/bootcamp/dsmovie/controller/MovieController.java
+++ backend-dsmovie/src/main/java/br/com/bootcamp/dsmovie/controller/MovieController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/movies")
@@ -21,5 +19,11 @@ public class MovieController {
     public ResponseEntity<Page<MovieDTO>> findAllPaged(Pageable pageable) {
         Page<MovieDTO> moviesPaged = movieService.findAllPaged(pageable);
         return ResponseEntity.ok().body(moviesPaged);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+        MovieDTO movieDTO = movieService.findById(id);
+        return ResponseEntity.ok().body(movieDTO);
     }
 }
--- backend-dsmovie/src/main/java/br/com/bootcamp/dsmovie/services/MovieService.java
+++ backend-dsmovie/src/main/java/br/com/bootcamp/dsmovie/services/MovieService.java
@@ -3,11 +3,14 @@ package br.com.bootcamp.dsmovie.services;
 import br.com.bootcamp.dsmovie.dto.MovieDTO;
 import br.com.bootcamp.dsmovie.entities.Movie;
 import br.com.bootcamp.dsmovie.repositories.MovieRepository;
+import br.com.bootcamp.dsmovie.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -19,5 +22,12 @@ public class MovieService {
     public Page<MovieDTO> findAllPaged(Pageable pageable) {
         Page<Movie> movies = movieRepository.findAll(pageable);
         return movies.map(x -> new MovieDTO(x));
+    }
+
+    @Transactional
+    public MovieDTO findById(Long id) {
+        Optional<Movie> movieOptional = movieRepository.findById(id);
+        Movie movie = movieOptional.orElseThrow(() -> new ResourceNotFoundException("Filme não encontrado."));
+        return new MovieDTO(movie);
     }
 }
