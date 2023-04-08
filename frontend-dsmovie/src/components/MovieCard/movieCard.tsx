81d92734f70ff1c553f95db5f411e5ebb198f851
lhp97 <lhdprado@gmail.com>
2023-04-08 11:07:02 -0300

    Props

--- frontend-dsmovie/src/components/MovieCard/movieCard.tsx
+++ frontend-dsmovie/src/components/MovieCard/movieCard.tsx
@@ -1,15 +1,12 @@
 import MovieScore from "components/MovieScore/movieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
-  const movie = {
-    id: 1,
-    image:
-      "https://occ-0-3934-114.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABVWS9pqT4D1VP8vlUp4tpjXcO0T2xb7QHYJAZgmwalMcwqgHKJNUEZdbgwOrydpkKgTrgA-hRPGQh5XYsNydSOMvYCsV1Vnm808M.jpg?r=93a",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+type Props = {
+  movie: Movie;
+}
+
+function MovieCard( { movie } : Props) {
 
   return (
     <div>
